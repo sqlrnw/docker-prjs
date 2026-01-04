710c97de623647c5f893f0f18780d25bc2f4fa73
Mohamed Ragap <medoragap7@gmail.com>
2026-01-04 14:16:31 +0200

    the first commit for prj1

---
+++ prj1-docker-node1/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello mohamed from Dockerized Node.js!",
+    port: PORT,
+    hostname: require("os").hostname()
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
